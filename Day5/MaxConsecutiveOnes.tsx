f43ad7dfc7ef157876a0c5b97b155d3806f9a39a
JANANI B <104265301+jananibalu@users.noreply.github.com>
2025-07-02 22:34:43 +0530

    Add files via upload

---
+++ Day5/MaxConsecutiveOnes.tsx
@@ -0,0 +1,21 @@
+function maxConsecutiveOnes(arr) {
+    let maxCount = 0;
+    let currentCount = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 1) {
+            currentCount++;
+            if (currentCount > maxCount) {
+                maxCount = currentCount;
+            }
+        } else {
+            currentCount = 0;
+        }
+    }
+
+    return maxCount;
+}
+
+
+const binaryArray = [1, 1, 0, 1, 1, 1];
+console.log(maxConsecutiveOnes(binaryArray));
